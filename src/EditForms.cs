a9968f1817fdac5ae31f7dafd4aa7ac8394ec0d5
lizaavdonina <elizavetaavdonina511@gmail.com>
2025-10-15 12:56:55 +0300

    Update EditForms.cs

--- src/EditForms.cs
+++ src/EditForms.cs
@@ -1,5 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Металлопрокат
